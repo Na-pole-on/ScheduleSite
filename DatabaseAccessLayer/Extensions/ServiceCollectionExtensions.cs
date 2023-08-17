58546ecdc9cc49e0f0e20c38378a40a171692ae9
Na-pole-on <juikomathew@gmail.com>
2023-08-17 19:17:56 +0300

    add extensions

---
+++ DatabaseAccessLayer/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAccessLayer.Extensions
+{
+    internal class ServiceCollectionExtensions
+    {
+    }
+}
