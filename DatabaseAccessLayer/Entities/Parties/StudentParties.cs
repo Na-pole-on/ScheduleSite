1fe94a6fe4961cc252166558e8ba8d967bb1f431
Na-pole-on <juikomathew@gmail.com>
2023-08-17 18:30:48 +0300

    add all entities

---
+++ DatabaseAccessLayer/Entities/Parties/StudentParties.cs
@@ -0,0 +1,20 @@
+﻿using DatabaseAccessLayer.Entities.Profiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAccessLayer.Entities.Parties
+{
+    public class StudentParties
+    {
+        public string? Id { get; set; }
+
+        public Student? Student { get; set; }
+        public string? UserName { get; set; }
+
+        public Party? Party { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ DatabaseAccessLayer/Entities/Profiles/Teacher.cs
@@ -0,0 +1,14 @@
+﻿using DatabaseAccessLayer.Entities.Parties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAccessLayer.Entities.Profiles
+{
+    public class Teacher: User
+    {
+        public IEnumerable<Party>? Parties { get; set; }
+    }
+}
